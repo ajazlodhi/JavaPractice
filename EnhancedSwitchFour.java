6190b546aadcd1c420d9a6c13a6130070387391b
ajazlodhi <150616811+ajazlodhi@users.noreply.github.com>
2024-01-03 15:14:39 -0500

    Create EnhancedSwitchFour.java

---
+++ EnhancedSwitchFour.java
@@ -0,0 +1,22 @@
+public class EnhancedSwitchFour {
+    public static void main(String[] args) {
+        String username = "Steve";
+        String password = getPassword(username);
+        System.out.println("Password for " + username + "is :" + password);
+
+    }
+    public static String getPassword(String username){
+        String password;
+        password = switch (username) {
+            case "Alan" -> "A123";
+            case "John" -> "J520";
+            case "Steve" -> "S999";
+            default -> {
+                password = "Invalid User Name";
+                yield password;
+            }
+        };
+        return password;
+
+    }
+}
