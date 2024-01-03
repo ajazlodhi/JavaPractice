2d5aba504dcb8ff266f0723a8ce4ed58e566e0f3
ajazlodhi <150616811+ajazlodhi@users.noreply.github.com>
2024-01-03 17:29:21 -0500

    Update EnhancedSwitchFour.java

--- EnhancedSwitchFour.java
+++ EnhancedSwitchFour.java
@@ -1,22 +1,9 @@
-public class EnhancedSwitchFour {
+public class SwitchStatement{
     public static void main(String[] args) {
-        String username = "Steve";
-        String password = getPassword(username);
-        System.out.println("Password for " + username + "is :" + password);
-
-    }
-    public static String getPassword(String username){
-        String password;
-        password = switch (username) {
-            case "Alan" -> "A123";
-            case "John" -> "J520";
-            case "Steve" -> "S999";
-            default -> {
-                password = "Invalid User Name";
-                yield password;
-            }
-        };
-        return password;
+        int day = 3;
+        if (day == 1){
+            System.out.println("Monday");
+        }
 
     }
 }
