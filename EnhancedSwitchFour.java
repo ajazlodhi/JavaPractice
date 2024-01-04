e8ead180cda50c9324c13271e919f54b87ec93c9
ajazlodhi <150616811+ajazlodhi@users.noreply.github.com>
2024-01-03 19:23:25 -0500

    Update EnhancedSwitchFour.java

--- EnhancedSwitchFour.java
+++ EnhancedSwitchFour.java
@@ -1,10 +1,25 @@
-public class SwitchStatement{
-    public static void main(String[] args) {
-        int day = 7;
-        if (day == 1){
-            System.out.println("Sunday");
-        }
+public class MySwitchStatementTwo{
+        public static void main(String[] args) {
+            int number = 29;
+            String size;
+            switch (number){
+                case 29:
+                    size = "Small";
+                    break;
+                case 42:
+                    size = "Medium";
+                    break;
+                case 44:
+                    size = "Large";
+                    break;
+                case 48, 49, 50:
+                    size = "Extra Large";
+                    break;
+                default:
+                    size = "UNKNOWN";
+                    break;
+            }
+            System.out.println(size);
 
     }
 }
-
