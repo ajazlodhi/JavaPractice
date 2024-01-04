a82aa3018e6ce134a9017d5aee770250298ed233
ajazlodhi <150616811+ajazlodhi@users.noreply.github.com>
2024-01-03 19:16:15 -0500

    Update EnhancedSwitchFour.java

--- EnhancedSwitchFour.java
+++ EnhancedSwitchFour.java
@@ -1,8 +1,8 @@
 public class SwitchStatement{
     public static void main(String[] args) {
-        int day = 3;
+        int day = 7;
         if (day == 1){
-            System.out.println("Monday");
+            System.out.println("Sunday");
         }
 
     }
