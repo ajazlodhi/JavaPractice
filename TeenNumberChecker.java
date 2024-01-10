20d96ccd9792aa9d55a797294d68b2e92c4834e2
ajazlodhi <150616811+ajazlodhi@users.noreply.github.com>
2024-01-10 10:53:15 -0500

    Update TeenNumberChecker.java

--- TeenNumberChecker.java
+++ TeenNumberChecker.java
@@ -14,12 +14,15 @@ public class TeenNumberChecker{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the First Number : ");
+//       System.out.print("Enter the First Number : ");
         int num1 = scanner.nextInt();
-        System.out.print("Enter the Second Number : ");
+        num1 = 12;
+//        System.out.print("Enter the Second Number : ");
         int num2 = scanner.nextInt();
-        System.out.print("Enter the Third Number : ");
+        num2 = 15;
+//        System.out.print("Enter the Third Number : ");
         int num3 = scanner.nextInt();
+        num3 = 19;
         boolean isTeen = FindTeen(num1,num2,num3);
         if (isTeen){
             System.out.println("At Least one number is Teena");
