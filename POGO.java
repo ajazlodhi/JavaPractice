6fdd2fd1804016280527de9987170d2455fba336
ajazlodhi <150616811+ajazlodhi@users.noreply.github.com>
2024-01-26 15:45:41 -0500

    Update POGO.java

--- POGO.java
+++ POGO.java
@@ -4,14 +4,14 @@ import java.util.concurrent.Callable;
 public class POGO {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the Floor width : " );
-        double width = scanner.nextDouble();
-        System.out.print("Enter the Floor Length : " );
-        double length = scanner.nextDouble();
-        Floor floor = new Floor(width ,length );
-        System.out.print("Enter the Price per SQF : " );
-        double cost = scanner.nextDouble();
-        Carpet carpet = new Carpet(cost);
+//        System.out.print("Enter the Floor width : " );
+//        double width = scanner.nextDouble();
+//        System.out.print("Enter the Floor Length : " );
+//        double length = scanner.nextDouble();
+        Floor floor = new Floor(20 ,30 );
+//        System.out.print("Enter the Price per SQF : " );
+//        double cost = scanner.nextDouble();
+        Carpet carpet = new Carpet(4.5);
         double areaTotal = floor.getArea();
         System.out.println("Total SQFT Area SQF : " + areaTotal);
         Calculator calculator = new Calculator(floor,carpet);
